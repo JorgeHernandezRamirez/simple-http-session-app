46044934a28bbc97a156ee5e6072fcd9c6aefe99
jorgehernandezramirez <jorge.hernandez.ramirez@ibm.com>
2022-04-20 15:49:25 +0200

    first commit

--- external-session-web/src/main/java/com/ibm/external/session/web/ReadyServlet.java
+++ external-session-web/src/main/java/com/ibm/external/session/web/ReadyServlet.java
@@ -7,10 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/ready")
-public class K8sReadyServlet extends HttpServlet {
+public class ReadyServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
@@ -19,8 +18,6 @@ public class K8sReadyServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.println("OK");
+        resp.getWriter().println("OK");
     }
 }
--- external-session-web/src/main/java/com/ibm/external/session/web/SessionServlet.java
+++ external-session-web/src/main/java/com/ibm/external/session/web/SessionServlet.java
@@ -3,7 +3,6 @@ package com.ibm.external.session.web;
 import com.ibm.external.session.ejb.SessionEJB;
 
 import javax.ejb.EJB;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,49 +11,30 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Date;
 
 @WebServlet("/session")
 public class SessionServlet extends HttpServlet {
 
-  @EJB
-  SessionEJB sessionEJB;
+    @EJB
+    private SessionEJB sessionEJB;
 
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
-    final HttpSession session = request.getSession(true);
-    response.setContentType("text/html");
-    final PrintWriter out = response.getWriter();
-    final String title = "Simple HttpSession Demo";
-    String heading;
-    String serverIPAddress = "Unknown";
-    String serverHost = "Unknown";
-    Integer accessCount = new Integer(0);;
-    if (session.isNew()) {
-      heading = "Hello stranger";
-    } else {
-      heading = "Welcome back friend";
-      Integer oldAccessCount =
-        (Integer)session.getAttribute("accessCount");
-      if (oldAccessCount != null) {
-        accessCount =
-          new Integer(oldAccessCount.intValue() + 1);
-      }
-    }
-    session.setAttribute("accessCount", accessCount);
-
-
-    try {
-       InetAddress localhost = InetAddress.getLocalHost();
-       serverIPAddress = localhost.getHostAddress().trim();
-       serverHost = localhost.getHostName();
-    }
-    catch (UnknownHostException e) {
-        System.err.println("Error: cannot get server IP Address  : " + e.getMessage());
+    public void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws IOException {
+        final HttpSession session = request.getSession(true);
+        response.setContentType("text/html");
+        final PrintWriter out = response.getWriter();
+        final String title = "Simple HttpSession Demo";
+        final String heading = session.isNew() ? "Hello stranger" : "Welcome back friend";
+        final Integer accessCount = this.getAccessAccount(session);
+        session.setAttribute("accessCount", accessCount);
+        final InetAddress localhost = InetAddress.getLocalHost();
+        this.printOutput(out, title, heading, session, accessCount, localhost);
     }
 
-    out.println("<HTML><HEAD><TITLE>"+title+"</TITLE>\n" +
+    private void printOutput(final PrintWriter out, final String title, final String heading,
+                             final HttpSession session, final Integer accessCount, final InetAddress localhost) {
+        out.println("<HTML><HEAD><TITLE>" + title + "</TITLE>\n" +
                 "<meta http-equiv=\"Cache-Control\" content=\"no-cache, no-store, must-revalidate\">\n" +
                 "<meta http-equiv=\"Pragma\" content=\"no-cache\">\n" +
                 "<meta http-equiv=\"Expires\" content=\"0\">\n" +
@@ -84,21 +64,29 @@ public class SessionServlet extends HttpServlet {
                 "</TR>\n" +
                 "<TR>\n" +
                 "  <TD>POD Name</TD>\n" +
-                "  <TD>" + serverHost + "</TD>\n" +
+                "  <TD>" + localhost.getHostName() + "</TD>\n" +
                 "</TR>\n" +
                 "<TR>\n" +
                 "  <TD>POD IP address</TD>\n" +
-                "  <TD>" + serverIPAddress + "</TD>\n" +
+                "  <TD>" + localhost.getHostAddress().trim() + "</TD>\n" +
                 "</TR>\n" +
                 "</TABLE>\n" +
                 "</BODY></HTML>");
-      out.println("Value from EJB " + sessionEJB.count());
+        out.println("Value from EJB " + sessionEJB.count());
+    }
 
-  }
+    private Integer getAccessAccount(final HttpSession session) {
+        Integer accessCount = new Integer(0);
+        if (!session.isNew()) {
+            final Integer oldAccessCount = (Integer) session.getAttribute("accessCount");
+            if (oldAccessCount != null) {
+                accessCount = new Integer(oldAccessCount.intValue() + 1);
+            }
+        }
+        return accessCount;
+    }
 
-  public void doPost(HttpServletRequest request,
-                     HttpServletResponse response)
-      throws ServletException, IOException {
-    doGet(request, response);
-  }
+    public void doPost(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+        doGet(request, response);
+    }
 }
